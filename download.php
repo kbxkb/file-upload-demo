9ed11af617199769d3ca61523e22e8f34678f550
Ubuntu <Azure123@file-upload-demo.fwnbp3wokwfetkyef5fskuk0ra.xx.internal.cloudapp.net>
2016-12-08 19:55:07 +0000

    added encryption at rest

--- download.php
+++ download.php
@@ -1,4 +1,7 @@
 <?php
+
+define('FILE_ENCRYPTION_BLOCKS', 10000);
+
 function hasValue($input)
 {
         $strTemp = trim($input);
@@ -22,6 +25,43 @@ function encrypt_decrypt_short_string($key, $filename)
                 }
         }
         return $encrypted;
+}
+
+/**
+ * Decrypt the passed file and saves the result in a new file, removing the
+ * last 4 characters from file name.
+ * 
+ * @param string $source Path to file that should be decrypted
+ * @param string $key    The key used for the decryption (must be the same as for encryption)
+ * @param string $dest   File name where the decryped file should be written to.
+ * @return string|false  Returns the file name that has been created or FALSE if an error occured
+ */
+function decryptFile($source, $key, $dest)
+{
+    $key = substr(sha1($key, true), 0, 16);
+
+    $error = false;
+    if ($fpOut = fopen($dest, 'w')) {
+        if ($fpIn = fopen($source, 'rb')) {
+            // Get the initialzation vector from the beginning of the file
+            $iv = fread($fpIn, 16);
+            while (!feof($fpIn)) {
+                $ciphertext = fread($fpIn, 16 * (FILE_ENCRYPTION_BLOCKS + 1)); // we have to read one block more for decrypting than for encrypting
+                $plaintext = openssl_decrypt($ciphertext, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $iv);
+                // Use the first 16 bytes of the ciphertext as the next initialization vector
+                $iv = substr($ciphertext, 0, 16);
+                fwrite($fpOut, $plaintext);
+            }
+            fclose($fpIn);
+        } else {
+            $error = true;
+        }
+        fclose($fpOut);
+    } else {
+        $error = true;
+    }
+
+    return $error ? false : $dest;
 }
 
 $encrypted_filename = $_GET['file'];
@@ -54,12 +94,21 @@ if(!file_exists($file_path))
 	exit;
 }
 
-$fp = fopen($file_path, 'rb');
-header("Content-Type: application/octet-stream");
-header("Content-Transfer-Encoding: Binary");
-header("Content-disposition: attachment; filename=\"" . $uploaded_filename . "\""); 
-fpassthru($fp);
-fclose($fp);
-exit;
-
+$unencrypted_tmp_file = $file_path . ".tmp";
+if(!decryptFile($file_path, $key, $unencrypted_tmp_file))
+{
+	echo "There was an error decrypting your file on the server. Please contact the file owner.<br/><br/>";
+	exit;
+}
+else
+{
+	$fp = fopen($unencrypted_tmp_file, 'rb');
+	header("Content-Type: application/octet-stream");
+	header("Content-Transfer-Encoding: Binary");
+	header("Content-disposition: attachment; filename=\"" . $uploaded_filename . "\""); 
+	fpassthru($fp);
+	fclose($fp);
+	unlink($unencrypted_tmp_file);
+	exit;
+}
 ?>
--- upload.php
+++ upload.php
@@ -1,5 +1,7 @@
 <?php
 
+define('FILE_ENCRYPTION_BLOCKS', 10000);
+
 function hasValue($input)
 {
 	$strTemp = trim($input);
@@ -23,6 +25,43 @@ function encrypt_decrypt_short_string($key, $filename)
 		}
 	}
 	return $encrypted;
+}
+
+/**
+ * Encrypt the passed file and saves the result in a new file.
+ * 
+ * @param string $source Path to file that should be encrypted
+ * @param string $key    The key used for the encryption
+ * @param string $dest   File name where the encryped file should be written to.
+ * @return string|false  Returns the file name that has been created or FALSE if an error occured
+ */
+function encryptFile($source, $key, $dest)
+{
+    $key = substr(sha1($key, true), 0, 16);
+    $iv = openssl_random_pseudo_bytes(16);
+
+    $error = false;
+    if ($fpOut = fopen($dest, 'w')) {
+        // Put the initialzation vector to the beginning of the file
+        fwrite($fpOut, $iv);
+        if ($fpIn = fopen($source, 'rb')) {
+            while (!feof($fpIn)) {
+                $plaintext = fread($fpIn, 16 * FILE_ENCRYPTION_BLOCKS);
+                $ciphertext = openssl_encrypt($plaintext, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $iv);
+                // Use the first 16 bytes of the ciphertext as the next initialization vector
+                $iv = substr($ciphertext, 0, 16);
+                fwrite($fpOut, $ciphertext);
+            }
+            fclose($fpIn);
+        } else {
+            $error = true;
+        }
+        fclose($fpOut);
+    } else {
+        $error = true;
+    }
+
+    return $error ? false : $dest;
 }
 
 $target_dir = "/var/www/html/uploads";
@@ -83,24 +122,34 @@ else
 		$url = 'http://' . $_SERVER['SERVER_NAME'] . "/download.php?x=" . $time_now . "&file=" . $encrypted_filename;
 	}
 
-	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file))
+	$target_unencrypted_file = $target_file . ".tmp";
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_unencrypted_file))
 	{
-		echo "The file has been successfully uploaded<br/><br/>";
-		if(hasValue($passphrase))
+		if(!encryptFile($target_unencrypted_file, $time_now, $target_file))
 		{
-			echo "You can now share this link for downloading (<i>see note below</i>) -  <a href='$url'>$url</a><br/><br/>";
-			echo "<b>";
-			echo "CAUTION: Because you selected a password, the link above will NOT work unless the password is appended at the end of the link!<br/><br/>";
-			echo "The link is shown WITHOUT the actual password. Anyone in possession of the link must copy and paste the link on their browser address box,<br/>";
-			echo "followed by the password immediately after the link, which you must share with them.<br/><br/>";
-			echo "</b>";
-			echo "Please make a note of your password, the system does not store it, so we cannot retrieve if for you if you lose it:<br/>";
-			echo $passphrase;
+			echo "There was an error encrypting your file on the server. No file has been uploaded";
 		}
 		else
 		{
-			echo "You can now share this link for downloading: <a href='$url'>$url</a>";
+			unlink($target_unencrypted_file);
+			echo "The file has been successfully uploaded<br/><br/>";
+			if(hasValue($passphrase))
+			{
+				echo "You can now share this link for downloading (<i>see note below</i>) -  <a href='$url'>$url</a><br/><br/>";
+				echo "<b>";
+				echo "CAUTION: Because you selected a password, the link above will NOT work unless the password is appended at the end of the link!<br/><br/>";
+				echo "The link is shown WITHOUT the actual password. Anyone in possession of the link must copy and paste the link on their browser address box,<br/>";
+				echo "followed by the password immediately after the link, which you must share with them.<br/><br/>";
+				echo "</b>";
+				echo "Please make a note of your password, the system does not store it, so we cannot retrieve if for you if you lose it:<br/>";
+				echo $passphrase;
+			}
+			else
+			{
+				echo "You can now share this link for downloading: <a href='$url'>$url</a>";
+			}
 		}
+		unlink($target_unencrypted_file);
 	}
 	else
 	{
